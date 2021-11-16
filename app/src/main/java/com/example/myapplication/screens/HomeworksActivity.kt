3db9fef1896f0d3e5b3f2738bc20adfa6cc6b1cf
justin <justin.schramke@students.tbs1.de>
2021-11-16 11:55:27 +0100

    Classes and layouts added.

--- app/src/main/java/com/example/myapplication/screens/HomeworksActivity.kt
+++ app/src/main/java/com/example/myapplication/screens/HomeworksActivity.kt
@@ -1,4 +1,12 @@
 package com.example.myapplication.screens
 
-class HomeworksActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class HomeworksActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homeworks)
+    }
 }
